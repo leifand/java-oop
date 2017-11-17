3a4efeca2866c10a7589e771423875656506b5d0
Leif Anderson <leif.v.anderson@gmail.com>
2017-11-17 00:54:52 -0600

    initial commit

---
+++ Project.java
@@ -0,0 +1,21 @@
+public class Project {
+
+  private String name = new String();
+  private String description = new String();
+
+  public Project(String name) {
+    this.name = name;
+  }
+
+  public Project(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
+
+  public String getDescription() { return description; }
+  public void setDescription(name description) { this.description = description };
+
+}
---
+++ ProjectTest.java
@@ -0,0 +1,9 @@
+public class ProjectTest {
+
+  public static void main(String[] args) {
+
+    Project p1 = new Project();
+    Project p2 = new Project("Project X");
+    Project p3 = new Project("Project Y", "A super secret web site that counts pokes");
+  }
+}
